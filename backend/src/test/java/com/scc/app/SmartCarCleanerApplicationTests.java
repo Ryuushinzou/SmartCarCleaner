9ce5b250297baeb0378ae5d3ac2e8fbfc8532b13
Bogdan Andrei Alexandru <a.birsasteanu@gmail.com>
2020-04-18 21:48:48 +0300

    Update maven.yml

--- backend/src/test/java/com/scc/app/SmartCarCleanerApplicationTests.java
+++ backend/src/test/java/com/scc/app/SmartCarCleanerApplicationTests.java
@@ -2,23 +2,12 @@ package com.scc.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
 
 @SpringBootTest
 class SmartCarCleanerApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
-
-	@Test
-	void validateAddition() {
-		Assert.isTrue(sum(1, 2) == 3, String.format("Assertion failed; expected %d actual %d.", 3, sum(1, 2)));
-	}
-
-	@SuppressWarnings("Only created this for testing PR #30 - Restrict workflows to run on relevant file changes")
-	private int sum(int x0, int x1) {
-		return x0 + x1;
 	}
 
 }
